7b3df826a6f5567724cb95430b19caacb77b4e8c
jailkey <jan.kaufmann@netzproduktionen.de>
2014-01-04 12:19:25 +0100

    domscope-class refactored

--- Mold/Lib/DomScope.js
+++ Mold/Lib/DomScope.js
@@ -1,46 +1,22 @@
 Seed({
 		name : "Mold.Lib.DomScope",
-		dna : "class",
+		dna : "static",
 		include : [
-			"Mold.Lib.TemplateDirective"
+			"Mold.Lib.TemplateDirective",
+			"Mold.Lib.Template"
 		]
 	},
 	function(mainScope){
-		var _domScope = mainScope || document.createDocumentFragment();
-		var _directive = false;
-		this.publics = {
-			append : function(element){
-				if(Mold.isNodeList(element)){
-					
-				}
-				return this;
-			},
+		
+		var _mainTemplate = new Mold.Lib.Template(document.documentElement);
+		
+		return {
+
 			directive : function(directive){
-				switch(directive.at){
-					case "element":
-						var elements = _domScope.getElementsByTagName(directive.name);
-						break;
-					default:
-						break;
-				}
+				
 				Mold.Lib.TemplateDirective.add(directive);
-
-
-				if(Mold.isNodeList(elements)){
-					_domScope = elements;
-					console.log("is nodelist")
-				}else{
-					console.log("keine Nodelist")
-				}
-				return this;
-			},
-			collect : function(){
-
-				return collectionData;
-			},
-			get : function(){
-				return _domScope;
-			} 
+				_mainTemplate.refresh();
+			}
 		}
 	}
 );

d5dcb6a8f49ff0f6759a8123996d3dedbcef9c9c
jailkey <jan.kaufmann@netzproduktionen.de>
2014-01-08 23:21:50 +0100

    directives and filepreloading added

--- Mold/DNA/Component.js
+++ Mold/DNA/Component.js
@@ -4,23 +4,29 @@ Seed({
 		author : "Jan Kaufmann",
 		version : "0.0.1",
 		include : [
-			"Mold.Lib.Event",
 			"Mold.Lib.Component"
 		]
 	},
 	{
 		name :  "component",
 		dnaInit : function(){
-			
+			Mold.addLoadingProperty("controller");
 		},
 		createBy : "new",
 		create : function(seed) {
 			var target = Mold.createChain(Mold.getSeedChainName(seed));
 	
-			target[Mold.getTargetName(seed)] = function(){
-				var component = new Mold.Lib.Component(seed.element, seed.attributes, seed.func);
-				return component;
+			 
+			var component = new Mold.Lib.Component(seed.controller);
+			Mold.each(seed.func.directives, function(directive){
+				component.directive(directive);
+			});
+			if(seed.func.files){
+				component.files(seed.func.files);
 			}
+				
+			target[Mold.getTargetName(seed)] = component;
+			
 		}
 	}
 );

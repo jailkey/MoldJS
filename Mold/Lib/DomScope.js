1dbf62a01a561fb2cafbae627caee88f6b5672d2
jailkey <jan.kaufmann@netzproduktionen.de>
2014-01-08 23:27:55 +0100

    directives will now added direct to the document

--- Mold/Lib/DomScope.js
+++ Mold/Lib/DomScope.js
@@ -3,19 +3,57 @@ Seed({
 		dna : "static",
 		include : [
 			"Mold.Lib.TemplateDirective",
-			"Mold.Lib.Template"
+			"Mold.Lib.Template",
+			"Mold.Lib.CssParser"
 		]
 	},
 	function(mainScope){
 		
-		var _mainTemplate = new Mold.Lib.Template(document.documentElement);
+		//var _mainTemplate = new Mold.Lib.Template(document.documentElement);
+		var cssParser = new Mold.Lib.CssParser();
 		
+		Mold.Lib.TemplateDirective.on("directive.added", function(e){
+			var directive = e.data.directive;
+			switch(directive.at){
+				case "element":
+					var elements = document.getElementsByTagName(directive.name);
+					break;
+				case "attribute":
+					var elements = document.querySelectorAll("["+directive.name+"]");
+					break;
+				case "class":
+					console.log("get class directive")
+					var elements = document.getElementsByClassName(directive.name);
+					break;
+				case "style-property":
+					var elementStyles = cssParser.getElementsByStyleProperty(directive.name);
+					var elements = [];
+					var styleAttributes = [];
+					Mold.each(elementStyles, function(selected){
+						elements.push(selected.element)
+						styleAttributes.push(selected.properties)
+					});
+					break;
+				default:
+					break;
+			}
+			if(elements && elements.length){
+				var index = 0;
+				var template = false;
+				Mold.each(elements, function(element){
+					directive.apply(element, element, template, index, (styleAttributes) ? styleAttributes[index] : false);
+					index++;
+				});
+			}
+		});
+
 		return {
 
 			directive : function(directive){
-				
-				Mold.Lib.TemplateDirective.add(directive);
-				_mainTemplate.refresh();
+				cssParser.at("ready", function(){
+					Mold.Lib.TemplateDirective.add(directive);
+				});
+				//_mainTemplate.refresh();
 			}
 		}
 	}

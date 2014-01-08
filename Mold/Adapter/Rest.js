f974bd8ff27dc3d9f1b0d548cfa394bcce16f039
jailkey <jan.kaufmann@netzproduktionen.de>
2014-01-08 23:20:46 +0100

    trigger update event and parse JSON

--- Mold/Adapter/Rest.js
+++ Mold/Adapter/Rest.js
@@ -6,18 +6,24 @@ Seed({
 	function(config){
 
 		var _restpath = config.path;
-		//var _ajax = new Mold.Lib.Ajax();
-		//console.log("adapter", this);
+		var _that = this;
+		
 		this.on("ajax.error", function(e){
-			console.log("fehler", e)
-		})
+			
+		});
+
+		this.on("ajax.get.success", function(e){
+			_that.trigger("update", JSON.parse(e.data.xhr.responseText));
+		});
+
 		this.publics = {
 			save : function(data){
 				console.log("data", data)
-				this.send(_restpath, "data="+data, { method : "POST"});
+				this.send(_restpath+"?rand"+Math.random(), "data="+data, { method : "POST"});
 			},
 			load : function(id){
-				this.send(_restpath+id, false, { method : "GET"});
+				id = id || "";
+				this.send(_restpath+id+"?rand"+Math.random(), false, { method : "GET"});
 			},
 			remove : function(id){
 				this.send(_restpath+id, false, { method : "DELETE"});

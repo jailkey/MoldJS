e0fb6c6f5a9eae4856fb9c5e2ded71e2093abcf8
jailkey <jan.kaufmann@netzproduktionen.de>
2014-01-08 23:37:00 +0100

    Mold.Lib.File class added

---
+++ Mold/Lib/File.js
@@ -0,0 +1,46 @@
+"use strict";
+Seed({
+		name : "Mold.Lib.File",
+		dna : "class",
+		polymorph : true,
+		include : [
+			"Mold.Lib.Ajax",
+			"Mold.Lib.Event"
+		]
+	},
+	function(filepath){
+
+		var _content = false;
+		var _that = this;
+
+		Mold.mixing(this, new Mold.Lib.Event(this));
+
+		if(Mold.isNodeJS){
+
+		}else{
+			var ajax = new Mold.Lib.Ajax();
+			ajax.on("ajax.get.success", function(e){
+				_content = e.data.xhr.responseText;
+				_that.trigger("content.loaded", { content : _content})
+			});
+			ajax.on("ajax.error", function(){
+				_that.trigger("content.error");
+			});
+			ajax.send(filepath);
+		}
+
+	
+
+
+		this.publics = {
+			content : function(callback){
+				_that.at("content.loaded", function(e){
+					callback.call(null, e.data.content);
+				});
+			},
+			error : function(callback){
+				_that.at("content.error", callback);
+			}
+		}
+	}
+)

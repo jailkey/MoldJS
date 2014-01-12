6216d993f6ffc53fe08592e40a912bb55b26ad7a
jailkey <jan.kaufmann@netzproduktionen.de>
2014-01-12 14:04:05 +0100

    controller and view class created

--- Mold/DNA/Controller.js
+++ Mold/DNA/Controller.js
@@ -3,9 +3,7 @@ Seed({
 		dna : "dna",
 		author : "Jan Kaufmann",
 		include : [
-			"Mold.Lib.Event",
-			"Mold.Lib.GlobalEvents",
-			"Mold.Lib.Parents"
+			"Mold.Lib.Controller"
 		]
 	},
 	{
@@ -14,107 +12,17 @@ Seed({
 			Mold.addLoadingProperty("extend");
 		},
 		createBy : "new",
-		create : function(seed) {			
-				var target = Mold.createChain(Mold.getSeedChainName(seed));
-				//var events = false;
-				var controllerMainFunctions= function(){
-					this.ident = Mold.getId();
-					Mold.mixing(this, new Mold.Lib.Event(this));
-					Mold.mixing(this, new Mold.Lib.Parents());
-					this.registerd = [];
-					this.registerdParents = [];
-					var that = this;
-
-				
-					this.instanceOf = seed.name;
-
-					this.scope = false;
-
-					this.registerScope = function(scope){
-						console.log("register scope");
-						if(!this.scope){
-							this.scope = document.createDocumentFragment();
-						}
-						this.scope.appendChild(scope);
-					}
-
-					this.register = function(instance){
-						this.registerd.push(instance);
-						if(instance.scope){
-							this.registerScope(instance.scope);
-						}
-						instance.on("all", function(e){
-							that.trigger(e.event, e, { exclude : ["data", "destroy"] });
-						});
-						if(instance.addParent){
-							instance.addParent(that);
-						}
-						return instance;
-					}
-					
-					this.destroy = function(){
-						that.off();
-						that.trigger("destroy");
-						for(var i = 0; i < this.registerd.length; i++){
-							if(this.registerd[i].destroy){
-								this.registerd[i].destroy();
-							}
-						}
-						delete that;
-					}
-
-
-					
-					
-					return this;
-				}
-				
-				seed.func =	Mold.extend(controllerMainFunctions, seed.func,	{
-					superClassName : "rootclass",
-					sourceURL : seed.name
-				});
-		
-				target[Mold.getTargetName(seed)] = function(){
-					
-
-					var wrapper = Mold.wrap(seed.func, function(that){
-						if(that.publics){
-							for(var property in that.publics){
-								that[property] = that.publics[property];
-							}
-						}
-						delete that.publics;
-						return constructor;
-					})
-
-					var controller = new wrapper((arguments) ? arguments[0] : {});
-				
-					for(action in controller.actions){
-						controller.on(action.replace("@", ""), controller.actions[action]);
-					}
-					
-					if(arguments[0] && arguments[0].eventHandler){
-						arguments[0].eventHandler.at("all", function(e){
-								controller.trigger(e.event, e.data);
-							},
-							{
-								triggerElement : controller
-							}
-						);
-					}else{
-						Mold.Lib.GlobalEvents.at("all", function(e){
-								controller.trigger(e.event, e.data );
-							},
-							{
-								triggerElement : controller
-							}
-						);
-					}
-					
-					return controller;
-				}
-				
-				return target[Mold.getTargetName(seed)];
+		create : function(seed) {
+			var target = Mold.createChain(Mold.getSeedChainName(seed));
+			if(seed.func){
+				var controllerCode = seed.func;
+			}else if (seed.controller){
+				var controllerCode = seed.controller;
+			}
+			var controller = new Mold.Lib.Controller(controllerCode, seed.name);
+			target[Mold.getTargetName(seed)] = controller; 
+			
+			return target[Mold.getTargetName(seed)];
 				
 		}
 	}
--- Mold/DNA/View.js
+++ Mold/DNA/View.js
@@ -4,7 +4,8 @@ Seed({
 		author : "Jan Kaufmann",
 		version : "0.0.1",
 		include : [
-			"Mold.Lib.Event"
+			"Mold.Lib.Event",
+			"Mold.Lib.View"
 		]
 	},
 	{
@@ -14,108 +15,11 @@ Seed({
 		},
 		create : function(seed) {
 			
-				var target = Mold.createChain(Mold.getSeedChainName(seed));
-				var viewPrototype = function(){
-					var that = this;
-					var _modelRoutes = false;
-					Mold.mixing(this, new Mold.Lib.Event(that));
-					Mold.mixing(this, new Mold.Lib.Parents());
+			var target = Mold.createChain(Mold.getSeedChainName(seed));
 
-					this.template = false;
-
-					this.filter = false;					
-					
-					this.registerd = [];
-					this.register = function(instance){
-						that.registerd.push(instance);
-						instance.on("all", function(e){
-							that.trigger(e.event, e, { exclude : ["data", "destroy"] });
-						});
-						if(instance.addParent){
-							instance.addParent(this);
-						}
-						return instance;
-					}
-
-					this.addModelRoute = function(modelRoute){
-
-					}
-					
-					this.bind = function(model){
-						if(!_modelRoutes){
-							that.template.bind(model);
-						}
-					}					
-					
-					this.destroy = function(){
-						that.off();
-						that.trigger("destroy");
-						if(that.element.parentNode){
-							that.element.parentNode.removeChild(that.element);
-						}else{
-							console.log("ERROR View Element has no parentNode");
-						}
-						delete that;
-					}
-					//return this;
-				}
-
-
-				if(!seed.extend){
-					seed.func =	Mold.extend( viewPrototype, seed.func, {
-						superClassName : "rootclass",
-						sourceURL : seed.name
-					});
+			target[Mold.getTargetName(seed)] = new Mold.Lib.View(seed.func, seed.name, seed.extend);
 			
-				}else{
-					var superElement = Mold.getRawSeed(seed.extend);		
-					seed.func = Mold.extend(superElement.func, seed.func, {
-						sourceURL : seed.extend
-					});
-				}	
-	
-				
-				target[Mold.getTargetName(seed)] = function(){
-					var config = (arguments) ? arguments[0] : {};
-					var view = new seed.func(config);
-					if(!view.scope){
-						throw "a view needs the property 'scope' to work correct!"
-					}
-					
-					var getEventFunction = function(callback){
-						if(typeof callback=== "function"){
-							return function(e){
-								callback({ data : {
-									elementEvent : e, element : this
-								}});
-							}
-						}else{
-							var eventName = callback.replace("@", "");
-							return function(e){
-								view.trigger(eventName, { elementEvent : e, element : this});
-							}
-						}
-
-					}
-
-					for(var selector in view.events){
-						var selectedElements = view.element.querySelectorAll(selector);
-						for(var i = 0; i < selectedElements.length; i++){
-							for(selectedEvent in view.events[selector]){
-								view.appendChild(selectedElements[i]);
-								selectedElements[i].addEventListener(selectedEvent, function(){
-									return getEventFunction(view.events[selector][selectedEvent]);
-								}());
-							}
-						}
-					}
-					for(action in view.actions){
-						view.on(action.replace("@", ""), view.actions[action]);
-					}
-
-					return view;
-				}
-				return target[Mold.getTargetName(seed)];
+			return target[Mold.getTargetName(seed)];
 		}
 	}
 );

8fa076ad307c94d22bef85e6ae036dbb051bf7a1
jailkey <jan.kaufmann@netzproduktionen.de>
2014-04-05 11:19:46 +0200

    Loader now preloads svg, sync/asyc loading is now supported

--- Mold/Lib/Loader.js
+++ Mold/Lib/Loader.js
@@ -16,7 +16,7 @@ Seed({
 
 		var _getFileTyp = function(file){
 			var typs = {
-				image : [ "png", "jpg", "gif", "bmp" ],
+				image : [ "png", "jpg", "gif", "bmp", "svg" ],
 				script : [ "js" ],
 				style : [ "css", "scss" ]
 			}
@@ -52,8 +52,8 @@ Seed({
 			}
 		}
 
-		var _loadNextFile = function(){
-
+		var _loadNextFile = function(async){
+			
 			var selectedFile = _files.shift();
 			that.trigger("process", { len : _fileLen, _loaded : (_fileLen -_files.length), filename : selectedFile.name});
 			_isLoading = true;
@@ -61,7 +61,7 @@ Seed({
 			if(selectedFile.type === "script"){
 
 				Mold.loadScript(selectedFile.name, function(){
-					_checkLoaded();
+					_checkLoaded(async);
 				})
 
 			}else if(selectedFile.type === "image"){
@@ -69,17 +69,20 @@ Seed({
 				var img = new Image(),
 					imageEvents = new Mold.Lib.Event(img);
 
-				imageEvents.on("load", function(){
+				imageEvents.on("load", function(e){
+					console.log("successfully loaded", img);
 					_checkLoaded();
 				});
 
 				imageEvents.on("error", function(e){
 					that.trigger("error", e);
+					console.log("Error", e, img)
 					_isLoadingError = true;
-					_checkLoaded();
+					_checkLoaded(async);
 				});
-
+				
 				img.src = selectedFile.name;
+			//	console.log("add source", selectedFile.name, img);
 
 			}else if(selectedFile.type === "style"){
 
@@ -87,13 +90,13 @@ Seed({
 					styleEvent = new Mold.Lib.Event(styleSheet);
 
 				styleEvent.on("load", function(){
-					_checkLoaded();
+					_checkLoaded(async);
 				});
 
 				styleEvent.on("error", function(e){
 					that.trigger("error", e);
 					_isLoadingError = true;
-					_checkLoaded();
+					_checkLoaded(async);
 				});
 
 				styleSheet.type = "text/css";
@@ -101,6 +104,10 @@ Seed({
 				styleSheet.href = selectedFile.name;
 				document.getElementsByTagName("head")[0].appendChild(styleSheet);
 
+			}
+
+			if(async){
+				//_checkLoaded(async);
 			}
 			
 		}
@@ -113,15 +120,17 @@ Seed({
 			},
 			append : function(file){
 				if(Mold.isArray(file)){
-					_files.concat(file);
+					Mold.each(file, function(selected){
+						_files.push(_getFileObject(selected));
+					});
 				}else{
 					_files.push(_getFileObject(file));
 				}
 				return this;
 			},
-			load : function(){
+			load : function(async){
 				_fileLen = _files.length;
-				_loadNextFile();
+				_loadNextFile(async);
 				return this;
 			}
 

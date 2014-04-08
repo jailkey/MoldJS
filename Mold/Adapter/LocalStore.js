f56f9a28341efc63455750cd2fead11e65e074a8
jailkey <jan.kaufmann@netzproduktionen.de>
2014-04-08 21:18:29 +0200

    model adapter for local store added

---
+++ Mold/Adapter/LocalStore.js
@@ -0,0 +1,33 @@
+Seed({
+		name : "Mold.Adapter.LocalStore",
+		dna : "class",
+		implements : "Mold.Interface.ModelAdapter",
+		include : [
+			"Mold.Lib.Event",
+			"Mold.Lib.LocalStore"
+		]
+	},
+	function(){
+		
+		Mold.mixing(this, new Mold.Lib.Event(this));
+		var _localStore = new Mold.Lib.LocalStore();
+
+		this.publics = {
+			save : function(data, id){
+				return _localStore.save(data, id);
+			},
+			load : function(id){
+				var data =  _localStore.load(id);
+				this.trigger("update", { data : data, id : id });
+				return data;
+			},
+			add : function(data){
+				var id =  _localStore.add(data);
+				return id;
+			},
+			remove : function(id){
+				_localStore.remove(id);
+			}
+		}
+	}
+);

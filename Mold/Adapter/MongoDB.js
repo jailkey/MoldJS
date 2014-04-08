09eb41cd110650457288a4dd6c3f25ca59672189
jailkey <jan.kaufmann@netzproduktionen.de>
2014-04-08 21:23:18 +0200

    mongo db adapter added

---
+++ Mold/Adapter/MongoDB.js
@@ -0,0 +1,27 @@
+Seed({
+		name : "Mold.Adapter.MongoDB"
+		dna : "class",
+		include : [
+			"Mold.Lib.Event"
+		]
+	},
+	function(){
+
+
+		this.publics = {
+			save : function(data){
+				console.log("data", data)
+				this.send(_restpath+"?rand"+Math.random(), "data="+data, { method : "POST"});
+			},
+			load : function(id){
+				id = id || "";
+				this.send(_restpath+id+"?rand"+Math.random(), false, { method : "GET"});
+			},
+			remove : function(id){
+
+				this.send(_restpath+id, false, { method : "DELETE"});
+			},
+			add : function(data){
+		}	
+	}
+)

3bd398d3a92671df77533ba48392d31bd59cb06a
jailkey <jan.kaufmann@netzproduktionen.de>
2014-04-18 12:43:05 +0200

    DomScope renamend in Directive

--- Mold/Lib/Component.js
+++ Mold/Lib/Component.js
@@ -2,7 +2,7 @@ Seed({
 		name : "Mold.Lib.Component",
 		dna : "class",
 		include : [
-			"Mold.Lib.DomScope",
+			"Mold.Lib.Directive",
 			"Mold.Lib.Loader",
 			"Mold.Lib.Event"
 		]
@@ -30,7 +30,21 @@ Seed({
 		this.publics = {
 			directive : function(directive){
 				directive.seed = _seed;
-				Mold.Lib.DomScope.directive(directive);
+				Mold.Lib.Directive.add(directive, document);
+				
+				new Mold.Lib.Event(document).on("DOMNodeInserted", function(){
+					Mold.Lib.Directive.add(directive, document);
+				});
+
+
+				if(directive.at === "attribute"){
+					new Mold.Lib.Event(document).on("attribute.modified", function(e){
+						if(e.data.attr === directive.name){
+							Mold.Lib.Directive.add(directive, document);
+						}
+					});
+				}
+
 			},
 			files : function(file){
 				if(file && file.length > 0){

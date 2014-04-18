3b64856c2d60faed0b7b6f9afbc6158b4f2abf73
jailkey <jan.kaufmann@netzproduktionen.de>
2014-04-18 12:43:58 +0200

    selector for external stylesheets modified

--- Mold/Lib/CssParser.js
+++ Mold/Lib/CssParser.js
@@ -11,29 +11,32 @@ Seed({
 		Mold.mixing(this, new Mold.Lib.Event(this));
 
 
-		var _files = document.querySelectorAll('link[type="text/css"]'),
+		var _files = document.querySelectorAll('link[rel="stylesheet"]'),
 			_that = this,
 			_contents = "",
 			_fileCounter = 0,
 			_parsedContent = false;
 
-
 		Mold.each(_files, function(file){
-			var file = new Mold.Lib.File("style.css");
-			file.content(function(content){
-				_fileCounter++;
-				_contents += content;
-				if(_fileCounter === _files.length){
-					_that.trigger("ready");
-				}
-			});
+			if(Mold.isNode(file)){
+				var file = new Mold.Lib.File(file.getAttribute('href'));
 
-			file.error(function(){
-				_fileCounter++;
-				if(_fileCounter === _files.length){
-					_that.trigger("ready");
-				}
-			})
+				file.content(function(content){
+					_fileCounter++;
+					_contents += content;
+
+					if(_fileCounter === _files.length){
+						_that.trigger("ready");
+					}
+				});
+
+				file.error(function(){
+					_fileCounter++;
+					if(_fileCounter === _files.length){
+						_that.trigger("ready");
+					}
+				});
+			}
 		});
 
 		var _parseProperty = function(value){
@@ -107,6 +110,7 @@ Seed({
 		}
 
 		this.on("ready", function(e){
+
 			_parsedContent = _parseContent(_contents);
 		});
 
@@ -117,7 +121,8 @@ Seed({
 					Mold.each(selected.properties, function(selectedProperty, name){
 						if(name === property){
 							var elements = document.querySelectorAll(selector);
-							Mold.each(elements, function(element){
+							Mold.each(elements, function(element, elementNAme){
+								
 								output.push({ element : element, properties : selected.properties });
 							});
 						}

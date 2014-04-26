6aaae3d50f2944b4648c5cf6f071e27dac727af2
jailkey <jan.kaufmann@netzproduktionen.de>
2014-04-26 18:35:22 +0200

    Touch example modified

--- examples/using_touch/Mold/Main.js
+++ examples/using_touch/Mold/Main.js
@@ -8,7 +8,8 @@ Seed(
 		]
 	},
 	function(){
-		console.log("test")
+
+
 		var canvas = new Mold.Lib.Element("div");
 
 		document.querySelector("body").appendChild(canvas);
@@ -18,12 +19,35 @@ Seed(
 			width : "300"
 		});
 
-		canvas.on('touchstart', function(e){
-			//e.preventDefault();
-		});
-		canvas.on('touchmove', function(event) {
-			console.log("touchemove", event)
-			 event.preventDefault();
+		var touchi = new Mold.Lib.Touch(canvas);
+		
+		touchi.on("pinch.in", function(){
+			console.log("zomm in")
+		
+		}).on("pinch.out", function(){
+			console.log("zomm out")
+		
+		}).on("single.tap", function(e){
+			console.log("Single Tab")
+		
+		}).on("double.tap", function(e){
+			console.log("DoubleTab")
+		}).on("hold", function(){
+			console.log("HOLD");
+		}).on("swipe.right", function(){
+			console.log("Swipe right")
+		}).on("swipe.left", function(){
+			console.log("Swipe left")
+		}).on("swipe.up", function(){
+			console.log("Swipe up")
+		}).on("swipe.down", function(){
+			console.log("Swipe down")
+		}).on("rotate", function(){
+			console.log("rotate")
+		}).on("drag.down", function(){
+			console.log("drag down")
+		}).on("drag.up", function(){
+			console.log("drag up")
 		});
 
 	}

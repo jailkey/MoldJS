0c8def2f5601dc14c1fcb65dd142f65d93257407
jailkey <jan.kaufmann@netzproduktionen.de>
2014-08-01 11:03:49 +0200

    Url class added

---
+++ Mold/Lib/Url.js
@@ -0,0 +1,30 @@
+Seed({
+		name : "Mold.Lib.Url",
+		dna : "class"
+	},
+	function(url){
+
+		var _parameter = {};
+
+		if(!url){
+			if(!Mold.isNodeJS){
+				
+				Mold.each(window.location.search.replace("?", "").split("&"), function(parameter){
+					console.log("ADD", parameter.split("=")[0], parameter.split("=")[1])
+					_parameter[parameter.split("=")[0]] = parameter.split("=")[1];
+					
+				});
+				console.log("para", _parameter);
+			}else{
+				throw new Error("No url given!");
+			}
+		}
+
+		this.publics = {
+			parameter : function(){
+				console.log("PAREA;", _parameter);
+				return _parameter;
+			}
+		}
+	}
+)

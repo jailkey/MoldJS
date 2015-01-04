b7899cd69bdd804d963506f0c994a54e05b35f3a
jailkey <jan.kaufmann@netzproduktionen.de>
2015-01-04 11:44:13 +0100

    Rest server modified / use method for middleware added

--- Mold/Server/RestServer.js
+++ Mold/Server/RestServer.js
@@ -6,43 +6,53 @@ Seed(
 		include : [
 			"Mold.Lib.Event",
 			"Mold.Lib.Observer",
-			"Mold.Server.Session"
+			"Mold.Server.Session",
+			"Mold.Lib.Sequence"
 		],
 		description : "",
 		version : 0.1
 	},
 	function(ip, port, config){
 		
-		var http = require('http');
+		var http = Mold.node.require('http');
 		var that = this;
 		var _routes = [];
+		var _middlewares = [];
+
 		Mold.mixin(this, new Mold.Lib.Event(this));
+
+		var _getSequence = function(){
+
+		}
 		
 		var _startServer = function(){
 			process.on('uncaughtException', function (error) {
 			   console.log(error.stack);
 			});
-			try{
-			
-			http.createServer(function (req, res) {
-				var session = Mold.Server.Session.create(req);
-				that.trigger("ready", { request : req, result : res, session : session} );
-				if(!session.data.get("routes")){
-					session.data.set("routes", []);
-					Mold.each(_routes, function(route){
-						session.data.set("routes", new route(session));
-					});
-				}
-				var location =  {
-					pathname : req.url.split("?")[0],
-					search : (req.url.split("?").length > 1) ? req.url.split("?")[1] : "" ,
-					hash : ""
-				};			
-				
-				session.data.get("userevents").trigger("location.update", { location : location, request : req, response : res, session : session });
 
-			}).listen(port, ip);
-			
+			try{
+				http.createServer(function (req, res) {
+					var session = Mold.Server.Session.create(req);
+					var sequenze = new Mold.Lib.Sequence();
+
+					session.data.set('location', {
+						pathname : req.url.split("?")[0],
+						search : (req.url.split("?").length > 1) ? req.url.split("?")[1] : "" ,
+						hash : ""
+					})
+
+
+					Mold.each(_middlewares, function(middleware){
+						sequenze = sequenze.step(function(next){
+							middleware.call(null, req, res, session, next);
+						});
+					});
+
+
+				}).listen(port, ip);
+
+				console.log("server started")
+
 			}catch(e){
 				console.log("Fehler", e);
 			}
@@ -51,12 +61,20 @@ Seed(
 		Mold.Lib.Observer.on("redirect", function(){
 			//console.log("redirect");
 		});
-		console.log("start server");
 	
+		_startServer();
 		this.publics = {
 			start : _startServer,
-			addRoute : function(route){
+			route : function(route){
 				_routes.push(route);
+			},
+			use : function(middleware){
+				if(middleware.action){
+					_middlewares.push(middleware.action);
+				}else{
+					_middlewares.push(middleware);
+				}
+				return this;
 			}
 		}
 	}

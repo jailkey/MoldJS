1e68d8c2454db6db4b7d7bf887ff97e300e33330
jailkey <jan.kaufmann@netzproduktionen.de>
2015-01-04 11:35:01 +0100

    return function instead of setting seed in router

--- Mold/DNA/SessionRouter.js
+++ Mold/DNA/SessionRouter.js
@@ -11,14 +11,16 @@ Seed({
 		name : "sessionrouter",
 		create : function(seed){
 
-			var target = Mold.createChain(Mold.getSeedChainName(seed));
+			//var target = Mold.createChain(Mold.getSeedChainName(seed));
 	
-			target[Mold.getTargetName(seed)] = function (session){
-				var router = new Mold.Lib.UrlRouter(seed);
+			return function (session){
+				var router = new Mold.Lib.UrlRouter(seed.func);
 				router.initRoutes();
 				router.setEventObject(session.data.get("userevents"));
+
 				session.data.get("userevents").on("location.update", function(data){
-					router.setLocation(data.data.location );
+					router.setLocation(data.data.location);
+					console.log("LOCATION UPDATE")
 					router.setServerParameter(data.data.request, data.data.response, data.data.session);
 					router.initRoutes();
 				});

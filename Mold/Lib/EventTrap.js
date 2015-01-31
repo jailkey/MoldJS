86369778752367c24b0b7f3845d25b628e3b0572
jailkey <jan.kaufmann@netzproduktionen.de>
2015-01-31 12:34:31 +0100

    EventTrap added

---
+++ Mold/Lib/EventTrap.js
@@ -0,0 +1,36 @@
+Seed({
+		name : 'Mold.Lib.EventTrap',
+		dna : 'class',
+		include : [
+			'Mold.Lib.Event'
+		]
+	},
+	function(properties){
+
+		var _exec = false,
+			_that = this,
+			_note = false;
+		
+		Mold.mixin(this, new Mold.Lib.Event(this));
+
+		Mold.each(properties, function(value, name){
+			console.log('watch', properties, name)
+
+			value.on(name, function(){
+				_that.trigger('change');
+			});
+
+		});
+	
+
+		this.publics = {
+			note : function(callback){
+				_note = callback;
+				return this;
+			},
+			execute : function(data){
+				return _note.apply(this, arguments);
+			}
+		}
+	}
+)

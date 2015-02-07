0aba2d9cae51349a8896529dc803ac22881154b5
jailkey <jan.kaufmann@netzproduktionen.de>
2015-02-08 00:01:11 +0100

    onhaschange is now default

--- Mold/DNA/UrlRouter.js
+++ Mold/DNA/UrlRouter.js
@@ -16,12 +16,12 @@ Seed({
 				//Mold.DNA.UrlRouter.init(seed);
 				urlRouter.initRoutes();
 				if(typeof window != "undefined" && window){
-					if(seed.onhashchange && seed.onhashchange === "update"){
+					if(!seed.onhashchange || seed.onhashchange !== "disabled"){
 						window.addEventListener("hashchange", urlRouter.initRoutes, false);
 					}
 				}else if(Mold.isNodeJS){
 					Mold.Lib.Observer.sub("location.update", function(data){
-						urlRouter.setLocation(data.data.location );
+						urlRouter.setLocation(data.data.location);
 						urlRouter.setServerParameter(data.data.request, data.data.response, data.data.session);
 						urlRouter.initRoutes();
 					});

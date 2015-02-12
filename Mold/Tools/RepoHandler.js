f0ca7e6742da6e56c07def6ba2d93777e1e14050
jailkey <jan.kaufmann@netzproduktionen.de>
2015-02-12 12:08:37 +0100

    create not existing pathes

--- Mold/Tools/RepoHandler.js
+++ Mold/Tools/RepoHandler.js
@@ -7,27 +7,48 @@ Seed({
 		]
 	},
 	function(){
+
 		var local = Mold.LOCAL_REPOSITORY;
-		var external = Mold.EXTERNAL_REPOSITORY;
-		var fileSystem = require("fs");
-		var _that = this;
+			external = Mold.EXTERNAL_REPOSITORY;
+			fileSystem = require("fs");
+			_that = this;
+
+		var _createSeedPath = function(seedName, path, chmod){
+			var parts = seedName.split("/"),
+				seedPath = "";
+
+			chmod = chmod || 0744;
+
+			for(var i = 0; i < parts.length - 1; i++){
+				seedPath += parts[i] + "/";
+				if(!fileSystem.existsSync(path + seedPath)){
+					fileSystem.mkdirSync(path + seedPath, chmod)
+				}
+			}
+		}
 
 		this.publics = {
 			seedExists : function(seedName, isExternal){
-				var seedPath = seedName.replace(".", "/") + ".js"
+				var seedPath = Mold.getSeedPath(seedName);
 				if(isExternal){
 					return fileSystem.existsSync(external + seedPath);
 				}else{
 					return fileSystem.existsSync(local + seedPath);
 				}
 			},
+			createSeedPath : _createSeedPath,
 			addSeed : function(seedName, code, isExternal, overwrite){
-				var path = ((isExternal) ? external : local) + seedName.replace(".", "/") + ".js";
+				
+				var path = ((isExternal) ? external : local),
+					seedPath = Mold.getSeedPath(seedName);
+
+				_createSeedPath(seedPath, path);
 
 				return new Mold.Lib.Promise(function(success, error){
 					var exists =  _that.seedExists(seedName, isExternal);
+		
 					if(overwrite || !exists){
-						fileSystem.writeFile(path, code, function(err) {
+						fileSystem.writeFile(path + seedPath, code, function(err) {
 							if(err){
 								error(err);
 								return;

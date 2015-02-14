8cab2d602660ab6d37da00c1fea9030b8a57665a
jailkey <jan.kaufmann@netzproduktionen.de>
2015-02-14 11:34:00 +0100

    nom handling added

--- Mold/Tools/RepoHandler.js
+++ Mold/Tools/RepoHandler.js
@@ -4,13 +4,19 @@ Seed({
 		platform : "node",
 		include : [
 			"Mold.Lib.Promise"
-		]
+		],
+		npm : {
+			"npm" : ">=2.0.0",
+			"semver" : "*",
+		}
 	},
 	function(){
 
-		var local = Mold.LOCAL_REPOSITORY;
-			external = Mold.EXTERNAL_REPOSITORY;
-			fileSystem = require("fs");
+		var local = Mold.LOCAL_REPOSITORY,
+			external = Mold.EXTERNAL_REPOSITORY,
+			fileSystem = require("fs"),
+			npm = require("npm"),
+			semver = require("semver"),
 			_that = this;
 
 		var _createSeedPath = function(seedName, path, chmod){
@@ -61,6 +67,48 @@ Seed({
 						success("Seed  " + seedName +  " exists, not updated!");
 					}
 				})
+			},
+			npmExists : function(packageName, version){
+				return new Mold.Lib.Promise(function(success, error){
+					npm.load({}, function (er) {
+	  					if (er){
+	  						console.log("error");
+	  						error({ type : 3, message : "Can't load npm!" })
+	  					}
+	  		
+	  					npm.commands.list(['--verbose', packageName], true, function(err, data){
+	  						if(err){
+	  							error({ type : 4, message :  "npm " + packageName + " failure " + err }); 
+	  							return; 
+	  						}
+	  		
+	  						if(!data._found){
+	  							error({ type : 1, message : "npm " + packageName + " is not installed!"})
+	  							return;
+	  						}
+	  						
+	  						if(data.dependencies && data.dependencies[packageName]){
+		  						if(!semver.satisfies(data.dependencies[packageName].version, version)){
+		  							error({ type : 2, message : "npm '" + packageName + "' version does not match. Required is " + version + ". Please update npm!"})
+		  							return;
+		  						}
+		  					}
+	  					
+	  					});
+	  				});
+	  			});
+			},
+			addNpm : function(packageName, version, isGlobal){
+				return new Mold.Lib.Promise(function(success, error){
+					var globale = (isGlobal) ? "-g" : "";
+					npm.commands.install([packageName, globale], function (err, data) {
+						if(err){
+							error("Can not install npm '" + packageName + "' " + err);
+						}else{
+							success(data);
+						}
+					});
+				});
 			}
 		}
 	}

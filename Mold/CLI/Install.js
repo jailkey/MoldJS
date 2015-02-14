db96757ffbe08b5d9194be3acc3732af2d080afb
jailkey <jan.kaufmann@netzproduktionen.de>
2015-02-14 11:33:36 +0100

    nom handling added

--- Mold/CLI/Install.js
+++ Mold/CLI/Install.js
@@ -5,7 +5,10 @@ Seed({
 			"Mold.DNA.CLI",
 			"Mold.Tools.SeedHandler",
 			"Mold.Tools.RepoHandler"
-		]
+		],
+		npm : {
+			'npm' : '>=2.0.0'
+		}
 	},
 	{
 		command : "install",
@@ -23,6 +26,9 @@ Seed({
 			'--without-dependencies' : {
 				'description' : 'Install the seed without dependencies'
 			},
+			'--without-npm' : {
+				'description' : 'Install seeds without npm dependencies'
+			},
 			'--overwrite' : {
 				'description' : 'Overwrite existing seeds'
 			}
@@ -30,6 +36,7 @@ Seed({
 		execute : function(parameter, cli){
 
 			var fileSystem = require("fs"),
+				npm = require("npm"),
 				repo = parameter.repository || 'http://www.moldjs.de/repo/',
 				seedHandler = new Mold.Tools.SeedHandler(),
 				targetRepo = false,
@@ -42,8 +49,6 @@ Seed({
 				cli.showError("-name parameter must be set!");
 				return false;
 			}
-
-
 
 			if(globalInstall){
 				targetRepo = Mold.EXTERNAL_REPOSITORY;
@@ -58,7 +63,9 @@ Seed({
 					toAdd = [];
 
 				cli.write("\nInstall "+parameter.name+" with dependencies into: " + targetRepo + "\n");
-				cli.write("Overwrite existing \n")
+				if(overwrite){
+					cli.write("Overwrite existing seeds!\n")
+				}
 				Mold.each(info, function(value){
 					if(value.exists && !existing[value.name] && !overwrite){
 						cli.write(cli.COLOR_YELLOW + "  Skip " + value.name + ", it is already installed!" + cli.COLOR_RESET + "\n");
@@ -66,18 +73,38 @@ Seed({
 					}else{
 						toAdd.push(value);
 					}
+					
+					if(value.header.npm){
+						Mold.each(value.header.npm, function(version, name){
+							//console.log("npm", value, name);
+							repoHandler.npmExists(name, version).fail(function(err){
+								if(err.type === 1){
+									repoHandler.addNpm(name, false, globalInstall)
+										.then(function(data){
+											console.log(cli.COLOR_GREEN + "npm " + name + " successfully installed!" + cli.COLOR_RESET);
+										}).fail(function(installError){
+											cli.showError("npm problem in " + value.name + ": \n");
+											cli.showError(installError);
+										});
+								}else{
+									cli.showError(err.message)
+								}
+							});
+						});
+					}
 				});
 
 				Mold.each(toAdd, function(val){
-
 					repoHandler.addSeed(val.name, val.code, globalInstall, overwrite).then(function(success){
 						cli.write("  " +cli.COLOR_GREEN + success + cli.COLOR_RESET + "\n");
 					}).fail(function(err){
-						cli.showError(err)
-					})
+						cli.showError(err);
+					});
 					
 				});
-			;
+				
+
+
 
 			}).fail(function(err){
 				cli.showError(err)

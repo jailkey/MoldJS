f054e8ff5ffa01761b2e2ac2730e3f62293fd98e
jailkey <jan.kaufmann@netzproduktionen.de>
2015-03-10 09:11:26 +0100

    Package command completed

--- Mold/CLI/Packages.js
+++ Mold/CLI/Packages.js
@@ -4,19 +4,61 @@ Seed({
 		platform : "node",
 		include : [
 			"Mold.DNA.CLI",
+			"Mold.Lib.MultiLineString"
 		]
 	},
 	{
 		command : "package",
-		description : "Install seed from the given repository",
+		description : "Creates a package in the current folder.",
 		parameter : {
 			'-r' : {
 				'description' : 'Create packages recursive.'
 			}
 		},
 		execute : function(parameter, cli){
-			var currentDir = process.cwd();
+			var fs = require("fs"),
+				currentDir = process.cwd(),
+				files = fs.readdirSync(currentDir),
+				collected = [];
+
+			var _getMoldName = function(currentDir){
+				var parts = currentDir.split("/");
+				var output = [];
+				Mold.each(parts, function(value){
+					if(value === "Mold" || output.length){
+						output.push(value);
+					}
+				});
+
+				return output.join(".");
+			}
+
+			var name = _getMoldName(currentDir);
+			if(!name){
+				throw new Error("no mold path found, are you shure that the current dir is inside a mold repo?")
+			}
+			Mold.each(files, function(value){
+				if(Mold.endsWith(value, ".js") && !Mold.startsWith(value, '*')){
+					//value
+					console.log("Add file to package: " + value);
+					collected.push("." + value.replace(".js", ""));
+				}
+			});
+
+			var packageFile =
+					"Seed({\n"+
+					"\tname : '" + name + ".*',\n" +
+					"\tdna : 'package',\n" +
+					"\tinclude : " + JSON.stringify(collected, null, "\t\t") + "\n" +
+					"});"
 			
+			fs.writeFile("*.js", packageFile, function(err) {
+				if(err) {
+					throw new Error(err);
+				} else {
+					console.log(cli.COLOR_GREEN + "Package successfully created!" + cli.COLOR_RESET);
+				}
+			}); 
 		}
 	}
 );

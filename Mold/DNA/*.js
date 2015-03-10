ba7eb51b7faf460b6a8cb448c50912bf7bb5620f
jailkey <jan.kaufmann@netzproduktionen.de>
2015-03-10 09:12:10 +0100

    DNA index package added

---
+++ Mold/DNA/*.js
@@ -0,0 +1,17 @@
+Seed({
+	name : 'Mold.DNA.*',
+	dna : 'package',
+	include : [
+		".CLI",
+		".Component",
+		".Controller",
+		".Element",
+		".ExternalLib",
+		".Interface",
+		".Model",
+		".Plugin",
+		".SessionRouter",
+		".UrlRouter",
+		".View"
+]
+});

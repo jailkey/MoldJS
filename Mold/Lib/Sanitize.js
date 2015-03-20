ac5e370b1f175c22658136b01e16043bc4d8cccd
jailkey <jan.kaufmann@netzproduktionen.de>
2015-03-20 09:01:37 +0100

    Mold.Lib.Sanitize added

---
+++ Mold/Lib/Sanitize.js
@@ -0,0 +1,71 @@
+Seed({
+		name : "Mold.Lib.Sanitize",
+		dna : "class"
+	},
+	function(){
+
+		var _standardChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVW",
+			_standardNumbers = "0123456789",
+			_urlChars = "\/\?\#\-\_\:&\.",
+			_emailChars = "@\_\-\.";
+
+		var _exploitsEntrys = [
+			"javascript:",
+			"vbscript:",
+			"livescript:"
+		]
+
+		var _eventHandler = [
+			"onmouseover", "onError", "FSCommand", "onAbort", "onActivate", "onAfterPrint", "onAfterUpdate", "onBeforeActivate", "onBeforeCopy", "onBeforeCut",
+			"onBeforeDeactivate", "onBeforeEditFocus", "onBeforePaste", "onBeforePrint", "onBeforeUnload", "onBeforeUpdate", "onBegin", "onBlur", "onBounce", "onCellChange",
+			"onChange", "onClick", "onContextMenu", "onControlSelect", "onCopy", "onCut", "onDataAvailable", "onDataSetChanged", "onDataSetComplete", "onDblClick",
+			"onDeactivate", "onDrag", "onDragEnd", "onDragLeave", "onDragEnter", "onDragOver", "onDragDrop", "onDragStart", "onDrop", "onEnd", "onErrorUpdate",
+			"onFilterChange", "onFinish", "onFocus", "onFocusIn", "onFocusOut", "onHashChange", "onHelp", "onInput", "onKeyDown", "onKeyPress", "onKeyUp", "onLayoutComplete",
+			"onLoad", "onLoseCapture", "onMediaComplete", "onMediaError", "onMessage", "onMouseDown", "onMouseEnter", "onMouseLeave", "onMouseMove", "onMouseOut",
+			"onMouseOver", "onMouseUp", "onMouseWheel", "onMove", "onMoveEnd", "onMoveStart", "onOffline", "onOnline", "onOutOfSync", "onPaste", "onPause",
+			"onPopState", "onProgress", "onPropertyChange", "onReadyStateChange", "onRedo", "onRepeat", "onReset", "onResize", "onResizeEnd", "onResizeStart",
+			"onResume", "onReverse", "onRowsEnter", "onRowExit", "onRowDelete", "onRowInserted", "onScroll", "onSeek", "onSelect", "onSelectionChange", "onSelectStart",
+			"onStart", "onStop", "onStorage", "onSyncRestored", "onSubmit", "onTimeError", "onTrackChange", "onUndo", "onUnload", "onURLFlip", "seekSegmentTime"
+		]
+
+
+
+		var _removeNonWhitlistedChars = function(string, list){
+			 return str.replace(new RegExp('[^' + list + ']+', 'g'), '');
+		}
+
+		var _removeBlacklistedChars = function(string, list){
+			 return str.replace(new RegExp('[' + list + ']+', 'g'), '');
+		}
+
+		var _removeXSSExploits = function(html){
+
+		}
+
+		var _removeEventhandler = function(html){
+
+		}
+
+		var _sanitzeAttributeUrls = function(){
+
+		}
+
+
+		return {
+
+			whitelist : function(chars, list){
+				return _removeNonWhitlistedChars(string, list);
+			},
+			blacklist : function(chars, list){
+				return _removeBlacklistedChars(string, list);
+			},
+			url : function(url){
+				return _removeNonWhitlistedChars(url, _standardChars + _standardNumbers + _urlChars);
+			},
+			email : function(){
+				return _removeNonWhitlistedChars(url, _standardChars + _standardNumbers + _emailChars);
+			}
+
+		}
+	}
+)

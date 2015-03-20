8e9f6db383befe1420aa642c5e43d86561c7a61b
jailkey <jan.kaufmann@netzproduktionen.de>
2015-03-20 09:01:17 +0100

    de-\encoding methods added

--- Mold/Lib/Encode.js
+++ Mold/Lib/Encode.js
@@ -6,6 +6,12 @@ Seed({
 		
 		
 		return {
+			encodeUTF8 : function(raw){
+				return unescape(encodeURIComponent(raw));
+			},
+			decodeUTF8 : function(utf8){
+				return decodeURIComponent(escape(utf8));
+			},
 			encodeHTML : function(markup){
 				return markup.replace(/&/g, '&amp;')
 							.replace(/"/g, '&quot;')
@@ -17,6 +23,33 @@ Seed({
 				input = input.replace(/<br>/g, "\n");
 				return input;
 			},
+			decodeUnicode : function(input){
+				return unescape(JSON.parse('"' + input.replace('"', '\\"')) + '"');
+			},
+			encodeURl : function(input){
+				return encodeURIComponent(input);
+			},
+			decodeURL : function(input){
+				return decodeURIComponent(input);
+			},
+			encodeBase64 : function(input){
+				if(Mold.isNodeJs){
+					return new Buffer(input).toString('base64')
+				}
+				if(!window.btoa){
+					return input;
+				}
+				return  window.btoa(unescape(encodeURIComponent( input )));
+			},
+			decodeBase64 : function(input){
+				if(Mold.isNodeJs){
+					return new Buffer(input, 'base64').toString('utf8');
+				}
+				if(!window.atob){
+					return input
+				}
+				return  decodeURIComponent(escape(window.atob( input ))
+			}
 		}
 	}
 );

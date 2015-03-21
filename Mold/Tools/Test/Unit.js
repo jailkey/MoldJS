7d635dcda764e2446b7b399ad6b469138f87d5aa
jailkey <jan.kaufmann@netzproduktionen.de>
2015-03-21 16:46:40 +0100

    automatic testing added

---
+++ Mold/Tools/Test/Unit.js
@@ -0,0 +1,67 @@
+Seed({
+		name : "Mold.Tools.Test.Unit",
+		dna : "action",
+		include : [
+			"Mold.Tools.Test.TDD",
+			"Mold.Tools.Test.ConsoleReporter"
+		]
+	},
+	function(){
+
+		var simpleUnit = new Mold.Tools.Test.TDD();
+		simpleUnit.addReporter(new Mold.Tools.Test.ConsoleReporter());
+		simpleUnit.test(function(){
+			describe("My First Test", function(){
+
+				var test = "";
+				before("before testing", function(done){
+					test = "to was rein"
+					done();
+				})
+
+				after("TESCHT NACHHER", function(done){
+					setTimeout(function(){
+						done();
+					}, 500)
+				})
+				
+				describe("My sub Test", function(){
+					it("sub sub Test something", function(){
+						var hasn;
+						expect(hasn).toBeDefined();
+					})
+				});
+
+				it("Test aync test", function(done){
+					console.log("mach es anycron");
+					done();
+				})
+
+				it("noch ein Test lala", function(){
+					console.log("mach was anderes");
+				})
+			});
+		});
+		simpleUnit.run();
+		
+		Mold.addPostProcessor("unittest", function(createdSeed, rawSeed){
+			if(rawSeed.test){
+				if(typeof rawSeed.test === "function"){
+					
+
+					
+				}
+			}
+		});
+
+		//if not nodejs run default tests
+		if(!Mold.isNodeJS){
+			Mold.load({
+				name : "Mold.Lib.*"
+			}).bind(function(){
+				//console.log("Package loaded!")
+			});
+		}
+
+	}
+);

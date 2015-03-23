c35276735b7907d69b435cc8e157cceb96233cfd
jailkey <jan.kaufmann@netzproduktionen.de>
2015-03-23 15:10:29 +0100

    Test for sanitizer added

--- Mold/Test/Lib/Sanitize.js
+++ Mold/Test/Lib/Sanitize.js
@@ -27,7 +27,153 @@ Seed({
 				expect(result).toBe("/hans/testmann/?asd/.&nas");
 			})
 
+			describe(".html()", function(){
 
+				it("remove script injection", function(){
+					var markup = '<SCRIPT SRC=http://ha.ckers.org/xss.js></SCRIPT>';
+					var result = san.html(markup);
+					expect(result).toBe("");
+				});
+
+				it("remove image XSS javascript directive", function(){
+					var markup = '<IMG SRC="javascript:alert(\'XSS\');">';
+					var result = san.html(markup);
+					expect(result).toBe('<IMG SRC="alertXSS" >');
+				});
+
+				it("remove non qoute XSS directive", function(){
+					var markup = '<IMG SRC=javascript:alert(\'XSS\')>';
+					var result = san.html(markup);
+					expect(result).toBe('<IMG SRC="alertXSS" >');
+				});
+
+				it("remove case insnsitive directive", function(){
+					var markup = '<IMG SRC=JaVaScRiPt:alert(\'XSS\')>';
+					var result = san.html(markup);
+					expect(result).toBe('<IMG SRC="alertXSS" >');
+				});
+
+				it("remove case insnsitive directive", function(){
+					var markup = '<IMG SRC=JaVaScRiPt:alert(\'XSS\')>';
+					var result = san.html(markup);
+					expect(result).toBe('<IMG SRC="alertXSS" >');
+				});
+
+
+				it("remove html entitys", function(){
+					var markup = '<IMG SRC=javascript:alert(\"XSS\")>';
+					var result = san.html(markup);
+					expect(result).toBe('<IMG SRC="alertXSS" >');
+				});
+
+				it("remove grave accent obfuscation", function(){
+					var markup = '<IMG SRC=`javascript:alert("RSnake says, \'XSS\'")`>';
+					var result = san.html(markup);
+					expect(result).toBe('<IMG SRC="alertRSnakesaysXSS" >');
+				});
+
+				it("remove grave accent obfuscation", function(){
+					var markup = '<IMG SRC=`javascript:alert("RSnake says, \'XSS\'")`>';
+					var result = san.html(markup);
+					expect(result).toBe('<IMG SRC="alertRSnakesaysXSS" >');
+				});
+
+				it("remove event handler", function(){
+				
+					var markup = '<a onmouSeoVer="alert(document.cookie)">xxs link</a>';
+					var result = san.html(markup);
+					expect(result).toBe('<a ="alert(document.cookie)">xxs link</a>');
+				})
+
+
+				it("remove src attribute by leaving it empty", function(){
+				
+					var markup = '<IMG SRC= onmouseover="alert(\'xxs\')">';
+					var result = san.html(markup);
+					expect(result).toBe('<IMG SRC= ="alert(\'xxs\')">');
+				})
+
+				it("remove decimal html character references", function(){
+				
+					var markup = '<IMG SRC=&#106;&#97;&#118;&#97;&#115;&#99;&#114;&#105;&#112;&#116;&#58;&#97;&#108;&#101;&#114;&#116;&#40;&#39;&#88;&#83;&#83;&#39;&#41;>';
+					var result = san.html(markup);
+					expect(result).toBe('<IMG SRC="alertXSS" >');
+				})
+
+				it("remove decimal html character references without trailing semicolon", function(){
+				
+					var markup = '<IMG SRC=&#106;&#97;&#118;&#97;&#115;&#99;&#114;&#105;&#112;&#116;&#58;&#97;&#108;&#101;&#114;&#116;&#40;&#39;&#88;&#83;&#83;&#39;&#41;>';
+					var result = san.html(markup);
+					expect(result).toBe('<IMG SRC="alertXSS" >');
+				})
+
+				it("remove decimal html character references without trailing semicolon", function(){
+				
+					var markup = '<IMG SRC=&#0000106&#0000097&#0000118&#0000097&#0000115&#0000099&#0000114&#0000105&#0000112&#0000116&#0000058&#0000097&#0000108&#0000101&#0000114&#0000116&#0000040&#0000039&#0000088&#0000083&#0000083&#0000039&#0000041>';
+					var result = san.html(markup);
+					expect(result).toBe('<IMG SRC="alertXSS" >');
+				})
+
+				it("remove hexadecimal html character references without trailing semicolon", function(){
+				
+					var markup = '<IMG SRC=&#x6A&#x61&#x76&#x61&#x73&#x63&#x72&#x69&#x70&#x74&#x3A&#x61&#x6C&#x65&#x72&#x74&#x28&#x27&#x58&#x53&#x53&#x27&#x29>';
+					var result = san.html(markup);
+					expect(result).toBe('<IMG SRC="alertXSS" >');
+				})
+
+				it("remove break up xss", function(){
+				
+					var markup = '<IMG SRC="jav	ascript:alert(\'XSS\');">';
+					var result = san.html(markup);
+					expect(result).toBe('<IMG SRC="alertXSS" >');
+				})
+
+				it("remove embeded encode xss", function(){
+				
+					var markup = '<IMG SRC="jav&#x09;ascript:alert(\'XSS\');">';
+					var result = san.html(markup);
+					expect(result).toBe('<IMG SRC="alertXSS" >');
+				})
+
+				it("remove embeded newline", function(){
+				
+					var markup = '<IMG SRC="jav&#x0A;ascript:alert(\'XSS\');">';
+					var result = san.html(markup);
+					expect(result).toBe('<IMG SRC="alertXSS" >');
+				})
+
+				it("remove embeded carriage return", function(){
+					var markup = '<IMG SRC="jav&#x0D;ascript:alert(\'XSS\');">';
+					var result = san.html(markup);
+					expect(result).toBe('<IMG SRC="alertXSS" >');
+				})
+
+				it("remove spaces and meta chars", function(){
+					var markup = '<IMG SRC=" &#14;  javascript:alert(\'XSS\');">';
+					var result = san.html(markup);
+					expect(result).toBe('<IMG SRC="alertXSS" >');
+				})
+
+				it("remove extra braked xss", function(){
+					var markup = '<<SCRIPT>alert("XSS");//<</SCRIPT>';
+					var result = san.html(markup);
+					expect(result).toBe('<');
+				})
+
+				it("remove xss from none unclosed tags", function(){
+					var markup = '<IMG SRC="javascript:alert(\'XSS\')"';
+					var result = san.html(markup);
+					expect(result).toBe('<IMG SRC="alert(\'XSS\')"');
+				})
+
+				it("remove unicode xss exploids", function(){
+					var markup = "<DIV STYLE=\"background-image:\0075\0072\006C\0028'\006a\0061\0076\0061\0073\0063\0072\0069\0070\0074\003a\0061\006c\0065\0072\0074\0028.1027\0058.1053\0053\0027\0029'\0029\">";
+					var result = san.html(markup);
+					expect(result).toBe('<IMG SRC="alert(\'XSS\')"');
+				})
+
+
+			});
 			
 		});	
 		

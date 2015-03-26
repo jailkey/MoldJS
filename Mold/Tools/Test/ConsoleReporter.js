c04209fad617c6ec185a529f9c98c1a978788405
jailkey <jan.kaufmann@netzproduktionen.de>
2015-03-26 16:12:32 +0100

    output format changed

--- Mold/Tools/Test/ConsoleReporter.js
+++ Mold/Tools/Test/ConsoleReporter.js
@@ -17,30 +17,68 @@ Seed({
 		}
 
 		var _error = function(value, level){
-			console.log("%c" + _tabsByLevel(level) + "ERROR: " + value, "color: #ff0000");
+			console.log("%c" + _tabsByLevel(level) + "ERROR: " + value, "color: #ff0000; font-weight: bold;");
 		}
 
 		var _success = function(value, level){
-			console.log("%c" + _tabsByLevel(level) +  "SUCCESS: " + value, "color: #00ff00");
+			console.log("%c" + _tabsByLevel(level) +  " " + value, "color: #00bb00; font-weight: bold;");
+		}
+
+		var _getInfo = function(entry, output){
+			if(!output){
+				var output = {
+					hasErrors : false,
+					errorCount : 0,
+					successCount : 0,
+					time : 0,
+					tests : 0
+				}
+			}
+			output.tests++;
+			output.time += entry.executionTime;
+			if(entry.error){
+				output.hasError = true;
+				output.errorCount++;
+			}else{
+				output.successCount++;
+			}
+			if(entry.children.length){
+				Mold.each(entry.children, function(subentry){
+					output = _getInfo(subentry, output);
+				});
+			}
+			return output;
 		}
 
 		var _iterateResult = function(result, level){
 			Mold.each(result.children, function(child){
-				var time = " - " + Math.round(child.executionTime) + "ms";
+				
+				var info = _getInfo(child);
+				var time = " - " + Math.round(info.time) + "ms";
 				if(child.description){
-					_info(child.description + time, level);
+					message = child.description + " (" + info.successCount + "/" + info.errorCount  + ")" + time;
 				}else{
-					_info(child.type + time, level);
+					message = child.type +  " (" + info.successCount + "/" + info.errorCount + ")"+ time;
 				}
+
+				if(info.hasError){
+					_error(message, level);
+				}else{
+					_success(message, level)
+				}
+
 				if(child.success){
-					_success("Test successfully!", level + 1);
+					//_success("Test successfully!", level + 1);
 				}else if(child.error){
 					_error(child.error, level + 1);
 				}
-
-				if(child.children.length){
-					_iterateResult(child, level + 1);
+				if(info.hasError){
+				
+					if(child.children.length){
+						_iterateResult(child, level + 1);
+					}
 				}
+				
 				
 			});
 		}

c73307dc205e6aff816bd7802f29b11de7867896
jailkey <jan.kaufmann@netzproduktionen.de>
2015-03-31 11:36:59 +0200

    Each method changed

--- Mold/Lib/Elements.js
+++ Mold/Lib/Elements.js
@@ -12,6 +12,7 @@ Seed ({
 		var collection = [];
 		var that = this;
 
+
 		Mold.each(selection, function(element, name){
 			if(name !== "length"){
 				collection.push(new Mold.Lib.Element(element));
@@ -19,6 +20,33 @@ Seed ({
 		});
 
 		if(collection.length > 0){
+			var keys = Mold.keys(collection[0]);
+			
+			for(var i = 0; i < keys.length; i++){
+				var name = keys[i];
+
+				try {				
+					var func = collection[0][name];
+				}catch(e){
+					var func = false;
+				}
+
+				
+				if(typeof func === "function"){
+					if(Mold.is(collection[0][name])){
+						collection[name] = function(){
+							var args = arguments;
+							Mold.each(collection, function(element){
+								if(typeof element[name] === "function"){
+									element[name].apply(that, args);
+								}
+							});
+							return collection;
+						}
+					}
+				}
+			}
+			/*
 			Mold.each(collection[0], function(func, name){
 				if(typeof func === "function"){
 					collection[name] = function(){
@@ -29,7 +57,7 @@ Seed ({
 						return collection;
 					}
 				}
-			});
+			}, false, true);*/
 		}else{
 			var copyElement = new Mold.Lib.Element('div');
 			Mold.each(copyElement, function(func, name){
@@ -40,7 +68,7 @@ Seed ({
 		}
 
 		collection.each = function(callback){
-			Mold.each(collection, callback);
+			Mold.each(collection, callback, collection);
 		}
 
 		collection.get = function(number){

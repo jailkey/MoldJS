150d0c90b9cc9c49e5473755c124e59ba573a715
jailkey <jan.kaufmann@netzproduktionen.de>
2015-04-06 11:50:28 +0200

    repo test function added

--- Mold/CLI/Test.js
+++ Mold/CLI/Test.js
@@ -6,7 +6,8 @@ Seed({
 			"Mold.Tools.RepoHandler",
 			"Mold.Tools.SeedParser",
 			"Mold.Tools.Test.Tester",
-			"Mold.Tools.Test.CLIReporter"
+			"Mold.Tools.Test.CLIReporter",
+			"Mold.Tools.ProjectHandler"
 		]
 	},
 	{
@@ -20,36 +21,44 @@ Seed({
 		execute : function(parameter, cli){
 			var fs = require("fs");
 
+			var project = new Mold.Tools.ProjectHandler();
+
 			//Test Local Repo
-			var repo = new Mold.Tools.RepoHandler(Mold.LOCAL_REPOSITORY);
-			repo.eachSeed(function(path){
-				if(fs.existsSync(path)){
-					var seedContent = fs.readFileSync(path);
-					if(seedContent !== ""){
-						var info = new Mold.Tools.SeedParser(seedContent);
-						if(info && info.header && info.header.test){
-							if(
-								!info.header.platform 
-								|| info.header.platform === "isomorph"
-								|| info.header.platform === "server"
-								|| info.header.platform === "node"
-							){
-								var tester = new Mold.Tools.Test.Tester();
-								tester.addReporter(new Mold.Tools.Test.CLIReporter(cli));
-								Mold
-									.load({ name : info.header.test })
-									.bind(function(){
-										Mold
-											.load({ name : info.name}).bind(function(){
-												tester.test(Mold.getSeed(info.header.test), Mold.getSeed(info.name));
-												tester.run();
-											});
-									})
+			var testRepo = function(repo, external){
+				var repo = new Mold.Tools.RepoHandler(Mold.LOCAL_REPOSITORY);
+				repo.eachSeed(function(path){
+					if(fs.existsSync(path)){
+						var seedContent = fs.readFileSync(path);
+						if(seedContent !== ""){
+							var info = new Mold.Tools.SeedParser(seedContent);
+							if(info && info.header && info.header.test){
+								if(
+									!info.header.platform 
+									|| info.header.platform === "isomorph"
+									|| info.header.platform === "server"
+									|| info.header.platform === "node"
+								){
+									var tester = new Mold.Tools.Test.Tester();
+									tester.addReporter(new Mold.Tools.Test.CLIReporter(cli));
+									Mold
+										.load({ name : info.header.test })
+										.bind(function(){
+											Mold
+												.load({ name : info.name}).bind(function(){
+													tester.test(Mold.getSeed(info.header.test), Mold.getSeed(info.name));
+													tester.run();
+												});
+										})
+								}
 							}
 						}
 					}
-				}
-			});
+				});
+			}
+
+			//test standard local
+			testRepo(Mold.LOCAL_REPOSITORY);
+			
 		}
 	}
 )

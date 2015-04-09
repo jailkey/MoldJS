419d6ea0e3d959f51a618b16670b2b1a0cae6fe9
jailkey <jan.kaufmann@netzproduktionen.de>
2015-04-09 13:39:45 +0200

    tests for mongo db added

--- Mold/Test/Lib/Mongo.js
+++ Mold/Test/Lib/Mongo.js
@@ -15,17 +15,87 @@ Seed({
 					});
 			});
 
-			it("insert a document in db", function(done){
+			it(".insert a document in db", function(done){
 				database
 					.insert("mycollection", { 'hans' : 'peter' })
 					.then(function(data){
-						console.log("my data", data)
-						done()
+						expect(data.result.ok).toBe(1);
+						done();
 					})
 					.fail(function(err){
 						console.log("FAILURE!", err)
 					})
 			});
+
+
+			it(".findOne dont finds a document", function(done){
+				database
+					.findOne("mycollection", { 'superman' : 'hero' })
+					.then(function(data){
+						expect(data).toBe(null);
+						done();
+					});
+			});
+
+			it(".findOne document", function(done){
+				database
+					.findOne("mycollection", { 'hans' : 'peter' })
+					.then(function(data){
+						expect(data.hans).toBe('peter');
+						done();
+					});
+			});
+
+			it(".update all documents", function(done){
+				database
+					.update("mycollection", { 'hans' : 'peter' }, { $set : { 'hans' : 'klaus' } }, { multi : true })
+					.then(function(data){
+						console.log("update result", data.result)
+						expect(data.result.ok).toBe(1);
+						done();
+					}).fail(function(){
+						console.log("FIAL")
+					});
+			});
+
+			it("test if all updateded found", function(done){
+				database
+					.findOne("mycollection", { 'hans' : 'klaus' })
+					.then(function(data){
+						expect(data.hans).toBe('klaus');
+						done();
+					});
+			});
+
+			it(".remove from database", function(done){
+				database
+					.remove("mycollection", { 'hans' : 'klaus' })
+					.then(function(data){
+						expect(data.result.ok).toBe(1);
+						done();
+					});
+			});
+
+			it("test if it is deleted", function(done){
+				database
+					.findOne("mycollection", { 'hans' : 'klaus' })
+					.then(function(data){
+						expect(data).toBe(null);
+						done();
+					});
+			});
+
+
+			it("close Database", function(done){
+				database.on("connection.closed", function(){
+					done();
+				})
+
+				database.close();
+			});
+
+
+
 		
 
 		});

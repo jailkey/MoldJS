6c28f3e1453f2c0f4896cf8b22721d0ce097b6a4
jailkey <jan.kaufmann@netzproduktionen.de>
2015-04-18 10:36:15 +0200

    Vdom Stringparser added

---
+++ Mold/Lib/VDom/StringParser.js
@@ -0,0 +1,151 @@
+Seed({
+		name : "Mold.Lib.VDom.StringParser",
+		dna : "static",
+		test : "Mold.Test.Lib.VDom.StringParser"
+	},
+	function(){
+
+		var VALUE_NODE = 1;
+		var BLOCK_NODE = 2;
+		var NEGATIVE_BLOCK_NODE = 3;
+
+		var BLOCK = "#";
+		var NEGATIVE_BLOCK = "^";
+		var BLOCK_END = "/";
+
+		return function(markup){
+
+			var length = markup.length,
+				i = 0,
+				actChar = false, 
+				lastChar = false,
+				previousChar = false,
+				writeState = 1,
+				twoBeforeChar = false,
+				twoAfterChar = false,
+				COLLECT = 1,
+				WRITE_VAR = 2,
+				WRITE_MARKUP = 3,
+				NOTHING = 4,
+				WRITE_BLOCK = 5,
+				COLLECT_BLOCK = 6,
+				collected = "",
+				START_BRACKEDS = "{{",
+				END_BRACKEDS = "}}",
+				parts = [],
+				blockCounter = 0,
+				nodeType = false,
+				blockName = false;
+
+			for(; i < length; i++){
+
+				actChar = markup[i];
+				nextChar = markup[i + 1];
+				twoAfterChar = markup[i + 2];
+
+				if(writeState !== COLLECT_BLOCK){
+					if(
+						actChar === START_BRACKEDS[0] || actChar === START_BRACKEDS[1]
+						|| actChar === END_BRACKEDS[0] || actChar === END_BRACKEDS[1]
+					){
+						writeState = NOTHING;
+					}else{
+						writeState = COLLECT;
+					}
+
+
+					if(actChar === START_BRACKEDS[0] &&  nextChar === START_BRACKEDS[1]){
+						writeState = WRITE_MARKUP;
+					}
+				}
+
+
+				if(actChar === END_BRACKEDS[0] &&  nextChar === END_BRACKEDS[1]){
+
+					if(Mold.startsWith(collected, BLOCK)){
+						writeState = COLLECT_BLOCK;
+						blockName = collected.replace("#", "");
+						nodeType = BLOCK_NODE; 
+						collected = "";
+						blockCounter++;
+
+					}else if(Mold.startsWith(collected, NEGATIVE_BLOCK)){
+						writeState = COLLECT_BLOCK;
+						blockName = collected.replace("^", "");
+						nodeType = NEGATIVE_BLOCK_NODE; 
+						collected = "";
+						blockCounter++;
+
+					}else if(Mold.endsWith(collected, BLOCK_END + blockName)){
+						if(blockCounter - 1 === 0){
+							writeState = WRITE_BLOCK;
+							collected = collected.substring(2, collected.length);
+							collected = collected.substring(0, collected.length - (START_BRACKEDS + BLOCK_END + blockName).length);
+						}
+						blockCounter--;
+
+					}else{
+						if(writeState !== COLLECT_BLOCK){
+							writeState = WRITE_VAR;
+						}
+					}
+					
+				}
+
+
+
+				if(writeState === COLLECT || writeState ===  COLLECT_BLOCK){
+					collected += actChar;
+				}
+
+
+
+				if(writeState === WRITE_VAR){
+					parts.push({
+						type : "node",
+						name : collected,
+						nodeType : VALUE_NODE
+					});
+					collected = "";
+				}
+
+				if(writeState === WRITE_BLOCK){
+					parts.push({
+						type : "node",
+						value : collected,
+						name : blockName,
+						nodeType : nodeType
+					});
+					collected = "";
+					blockName = "";
+				}
+
+				if(writeState === WRITE_MARKUP){
+					parts.push({
+						type : "markup",
+						value : collected
+					})
+					collected = "";
+				}
+
+				lastChar = actChar;
+				twoBeforeChar = lastChar;
+				lastWriteState = writeState;
+			}
+
+			if(collected !== ""){
+				parts.push({
+					type : "markup",
+					value : collected
+				})
+			}
+
+			if(blockCounter > 0){
+				throw new Error("Block validation error, some block maybe unclosed!");
+			}
+
+	
+			return parts;
+		}
+	}
+)

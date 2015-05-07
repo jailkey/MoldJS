29fdb906af0afe7facdb4b18df80a891fd8908d1
jailkey <jan.kaufmann@netzproduktionen.de>
2015-05-07 11:30:41 +0200

    attribute and string nodes added

--- Mold/Lib/VDom/NodeBuilder.js
+++ Mold/Lib/VDom/NodeBuilder.js
@@ -4,7 +4,8 @@ Seed({
 		include : [
 			{ BlockNode : "Mold.Lib.VDom.BlockNode" },
 			{ ValueNode : "Mold.Lib.VDom.ValueNode" },
-			{ DomNode : "Mold.Lib.VDom.DomNode" }
+			{ DomNode : "Mold.Lib.VDom.DomNode" },
+			{ StringNode : "Mold.Lib.VDom.StringNode" }
 		]
 	},
 	function(){
@@ -15,6 +16,8 @@ Seed({
 		var NEGATIVE_BLOCK_NODE = 3;
 		var ROOT_NODE = 4;
 		var DOM_NODE = 6;
+		var ATTR_NODE = 7;
+		var STRING_NODE = 8;
 
 		//document
 		var _doc = document;
@@ -30,52 +33,71 @@ Seed({
 			return vdom
 		}
 
-		return function(parts){
-			var vDomNodes = {};
+		var _addToCollection = function(collection, getArray, name, newNode){
+			if(getArray){
+				collection.push(newNode);
+			}else{
+				collection[name] = newNode;
+			}
+			return collection;
+		}
+
+
+		return function(parts, isStringNode, parent, getArray){
+			var vDomNodes = (getArray) ? [] : {};
 
 			for(;parts.length;){
-
 				var selected = parts.shift();
-			
+
 				if(selected.type === "node"){
 					switch(selected.nodeType){
 
 						case VALUE_NODE:
-				
-							vDomNodes[selected.name] = new ValueNode({
-								name : selected.name,
-								protoDom : _doc.createTextNode("pointer")
-							});
-					
 
+							_addToCollection(vDomNodes, getArray, selected.name, new ValueNode({
+								name : selected.name,
+								protoDom : (isStringNode) ? false : _doc.createTextNode("pointer"),
+								protoString : (isStringNode) ? selected.name : false,
+								parent : parent || false
+							}));
 							break;
+
 						case BLOCK_NODE:
-						
-							var vdom = _createProtoDom(selected.value);
 							
-							vDomNodes[selected.name] = new BlockNode({
+							var vdom = _createProtoDom(selected.value);
+							_addToCollection(vDomNodes, getArray, selected.name, new BlockNode({
 								name : selected.name,
 								content : selected.value,
 								type : selected.nodeType,
-								protoDom :  vdom[1]
-							});
+								protoDom :  vdom[1],
+								parent : parent || false
+							}));
 							break;
 					}
 				}else{
-					var dummy = _doc.createElement("div");
-					var i = 0;
-					dummy.innerHTML = selected.value;
-					var len = dummy.childNodes.length;
-					
-					for(; i < len; i++){
-						var name = "dom" + Mold.getId();
-						vDomNodes[name] = new DomNode({
-							name : name,
-							protoDom : dummy.childNodes[i]
-						});
-					}
-				
 
+					if(isStringNode){
+				
+						var name = "dom" + Mold.getId();
+						_addToCollection(vDomNodes, getArray, name, new StringNode({
+							name : name,
+							data : selected.value
+						}));
+
+					}else{
+
+						var dummy = _doc.createElement("div");
+						var i = 0;
+						dummy.innerHTML = selected.value;
+						var len = dummy.childNodes.length;
+						for(; i < len; i++){
+							var name = "dom" + Mold.getId();
+							_addToCollection(vDomNodes, getArray, name, new DomNode({
+								name : name,
+								protoDom : dummy.childNodes[i]
+							}));
+						}
+					}
 					
 				}
 			}

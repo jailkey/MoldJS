9685aa179be61b153b45a0a370f9134c8bd7fd3a
jailkey <jan.kaufmann@netzproduktionen.de>
2015-06-04 12:52:10 +0200

    use ProtoNode include

--- Mold/Lib/VDom/StringNode.js
+++ Mold/Lib/VDom/StringNode.js
@@ -4,52 +4,22 @@ Seed({
 	},
 	function(){
 
-		var STATE_RENDER = "render";
-		var STATE_NEW = "new";
-
 		return function StringNode(config){
-
-			config = config || {}
-
-			var _that = this;
-
-			this.nodeType = 8;
-			this.name = config.name || Mold.getId();
-			this.data = config.data || false;
-			this.parent = config.parent || false;
-			this.state = STATE_NEW;
-
+			//°include Mold.Lib.VDom.ProtoNode
+			
+			this.type = STRING_NODE;
 
 			this.clone = function(){
-				return new Mold.Lib.VDom.StringNode({
-					name : _that.name,
-					valueType : _that.valueType,
-					data : _that.data
-				})
-			}
-
-			this.setData = function(data){
-				_that.data = data;
-				_test = data;
-			}
-
-			this.render = function(){
-				_that.nodeValue = _that.data;
-				_that.protoDom.nodeValue = _that.data;
-				return _that.protoDom;
+				return new StringNode({
+					name : this.name,
+					data : this.data
+				});
 			}
 
 			this.renderString = function(){
-				_that.nodeValue = _that.data;
-				_that.protoString = _that.data;
-				return _that.protoString;
+				this.state = STATE_NO_CHANGES;
+				return this.data;
 			}
-
-			this.update = function(){
-				_that.nodeValue = _that.data;
-				_that.protoDom.nodeValue = _that.data;
-			}
-			
 		}
 	}
 )

87b084fdcfa94128a90bc4a6217d9f8f6519206c
jailkey <jan.kaufmann@netzproduktionen.de>
2015-06-04 12:53:18 +0200

    VDoc added

---
+++ Mold/Lib/VDom/VDoc.js
@@ -0,0 +1,12 @@
+Seed({
+		name : "Mold.Lib.VDom.VDoc",
+		dna : "static",
+		include : [
+			"->Mold.Lib.Dom"
+		]
+	},
+	function(){
+
+		return document // new Mold.Lib.Dom("<div></div>");
+	}
+)

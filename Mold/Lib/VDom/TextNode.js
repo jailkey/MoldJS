0982311b2983965229fc6cde9a6d6693cdb068b6
jailkey <jan.kaufmann@netzproduktionen.de>
2015-06-04 12:52:51 +0200

    TextNode added

---
+++ Mold/Lib/VDom/TextNode.js
@@ -0,0 +1,37 @@
+Seed({
+		name : "Mold.Lib.VDom.TextNode",
+		dna : "static",
+	},
+	function(){
+
+		return function TextNode(config){
+			//°include Mold.Lib.VDom.ProtoNode
+			
+			this.type = TEXT_NODE;
+			this.domPointer = false;
+
+			
+
+			this.clone = function(){
+
+				var newNode =  new TextNode({
+					name : this.name,
+					data : this.data
+				});
+
+				return newNode;
+			}
+
+			this.render = function(){
+
+				if(!this.domPointer ){
+					this.domPointer = _doc.createTextNode(this.data);
+				}
+				this.state = STATE_NO_CHANGES;
+				return this.domPointer;
+			}
+		}
+	
+	}
+	
+)

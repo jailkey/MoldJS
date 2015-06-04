bb99c0bb7a31f47ee49333bfb2f00d52a323870e
jailkey <jan.kaufmann@netzproduktionen.de>
2015-06-04 12:51:32 +0200

    modified for new node model

--- Mold/Lib/VDom/NodeBuilder.js
+++ Mold/Lib/VDom/NodeBuilder.js
@@ -14,23 +14,28 @@ Seed({
 		var VALUE_NODE = 1;
 		var BLOCK_NODE = 2;
 		var NEGATIVE_BLOCK_NODE = 3;
-		var ROOT_NODE = 4;
-		var DOM_NODE = 6;
-		var ATTR_NODE = 7;
+		var DOM_NODE = 5;
+		var TEXT_NODE = 6;
+		var ATTRIBUTE_NODE = 6;
 		var STRING_NODE = 8;
 
 		//document
 		var _doc = document;
 
-
-		var _createProtoDom = function(content){
-
+		var _createProtoDom = function(content, parent){
 			var containerNode = _doc.createElement('div');
 			containerNode.innerHTML = content;
-
-			var vdom = Mold.Lib.VDom.DomParser(containerNode.childNodes);
-
+			var vdom = Mold.Lib.VDom.DomParser(containerNode.childNodes, parent);
 			return vdom
+		}
+
+		var _createProtoString = function(content){
+			return Mold.Lib.VDom.NodeBuilder(
+					Mold.Lib.VDom.StringParser(content),
+					true,
+					false,
+					true
+			);
 		}
 
 		var _addToCollection = function(collection, getArray, name, newNode){
@@ -45,10 +50,9 @@ Seed({
 
 		return function(parts, isStringNode, parent, getArray){
 			var vDomNodes = (getArray) ? [] : {};
-
+	
 			for(;parts.length;){
 				var selected = parts.shift();
-
 				if(selected.type === "node"){
 					switch(selected.nodeType){
 
@@ -56,21 +60,23 @@ Seed({
 
 							_addToCollection(vDomNodes, getArray, selected.name, new ValueNode({
 								name : selected.name,
-								protoDom : (isStringNode) ? false : _doc.createTextNode("pointer"),
-								protoString : (isStringNode) ? selected.name : false,
-								parent : parent || false
+								parent : parent || false,
+								isString : isStringNode
 							}));
 							break;
 
 						case BLOCK_NODE:
-							
-							var vdom = _createProtoDom(selected.value);
+
+							if(isStringNode){
+								var vdom = _createProtoString(selected.value);
+							}else{
+								console.log("create block node", parent)
+								var vdom = _createProtoDom(selected.value, parent)[1];
+							}
 							_addToCollection(vDomNodes, getArray, selected.name, new BlockNode({
 								name : selected.name,
-								content : selected.value,
-								type : selected.nodeType,
-								protoDom :  vdom[1],
-								parent : parent || false
+								parent : parent || false,
+								isString : isStringNode
 							}));
 							break;
 					}
@@ -78,10 +84,11 @@ Seed({
 
 					if(isStringNode){
 				
-						var name = "dom" + Mold.getId();
+						var name = "string" + Mold.getId();
 						_addToCollection(vDomNodes, getArray, name, new StringNode({
 							name : name,
-							data : selected.value
+							data : selected.value,
+							isString : isStringNode
 						}));
 
 					}else{
@@ -91,10 +98,10 @@ Seed({
 						dummy.innerHTML = selected.value;
 						var len = dummy.childNodes.length;
 						for(; i < len; i++){
-							var name = "dom" + Mold.getId();
+							var name = "text" + Mold.getId();
 							_addToCollection(vDomNodes, getArray, name, new DomNode({
 								name : name,
-								protoDom : dummy.childNodes[i]
+								isString : isStringNode
 							}));
 						}
 					}

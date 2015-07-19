917ac39cc2a160883723f0e10a7272e50a032ba0
jailkey <jan.kaufmann@netzproduktionen.de>
2015-07-19 18:27:38 +0200

    Test for ObjectObserver added

---
+++ Mold/Test/Lib/ObjectObserver.js
@@ -0,0 +1,42 @@
+Seed({
+		name : "Mold.Test.Lib.ObjectObserver",
+		dna : "test"
+	},
+	function(ObjectObserver){
+
+		var testObject = false,
+			observer,
+			test = false;
+
+		describe("Test Mold.Lib.ObjectObserver", function(){
+
+			it("create test object", function(){
+				testObject = {
+					one : "irgendwas",
+					two : "wasanderes",
+					three : "nochwas"
+				}
+
+				observer = new ObjectObserver(testObject)
+			})
+
+			it("observe object property change", function(done){
+				test = function(data){
+					console.log("data", data)
+					expect(data.type).toBe("update");
+					expect(data.name).toBe("one");
+					expect(data.oldValue).toBe("irgendwas");
+					expect(data.object.one).toBe("six");
+					observer.unobserve(test);
+					done();
+				}
+
+				observer.observe(test)
+
+				testObject.one = "six";
+			});
+
+
+		});
+	}
+)

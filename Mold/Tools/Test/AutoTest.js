0b00f366928a7700faf00576338aa11ae4846a0f
jailkey <jan.kaufmann@netzproduktionen.de>
2015-08-17 08:58:12 +0200

    test only one seed functionality added

--- Mold/Tools/Test/AutoTest.js
+++ Mold/Tools/Test/AutoTest.js
@@ -3,25 +3,29 @@ Seed({
 		dna : "action",
 		include : [
 			"Mold.Tools.Test.Tester",
-			"Mold.Tools.Test.ConsoleReporter"
+			"Mold.Tools.Test.ConsoleReporter",
+			"Mold.Lib.Url"
 		]
 	},
 	function(){
-
+		var url = new Mold.Lib.Url();
+		var testSeed = url.parameter("test-seed");
 		Mold.addPostProcessor("unittest", function(createdSeed, rawSeed){
-			if(rawSeed.test){
-				var simpleUnit = new Mold.Tools.Test.Tester();
-				simpleUnit.addReporter(new Mold.Tools.Test.ConsoleReporter());
+			if(!testSeed || testSeed === rawSeed.name){
+				if(rawSeed.test){
+					var simpleUnit = new Mold.Tools.Test.Tester();
+					simpleUnit.addReporter(new Mold.Tools.Test.ConsoleReporter());
 					
-				if(typeof rawSeed.test === "function"){
-					simpleUnit.test(rawSeed.test, createdSeed);
-					simpleUnit.run();
-				}else if(Mold.startsWith(rawSeed.test, "Mold.")){
-					Mold.load({ name : (Mold.isExternalSeed(rawSeed.name) ? "->" : "") + rawSeed.test }).bind(function(){
-						var testSeed = Mold.getSeed(rawSeed.test);
-						simpleUnit.test(testSeed, createdSeed);
+					if(typeof rawSeed.test === "function"){
+						simpleUnit.test(rawSeed.test, createdSeed);
 						simpleUnit.run();
-					});
+					}else if(Mold.startsWith(rawSeed.test, "Mold.")){
+						Mold.load({ name : (Mold.isExternalSeed(rawSeed.name) ? "->" : "") + rawSeed.test }).bind(function(){
+							var testSeed = Mold.getSeed(rawSeed.test);
+							simpleUnit.test(testSeed, createdSeed);
+							simpleUnit.run();
+						});
+					}
 				}
 			}
 		});

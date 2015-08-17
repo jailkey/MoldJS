9e2aa0eb56049a13a9a7d419a3f5fb5c03fffe0f
jailkey <jan.kaufmann@netzproduktionen.de>
2015-08-17 08:57:53 +0200

    tests modified

--- Mold/Test/Lib/VDom/StringParser.js
+++ Mold/Test/Lib/VDom/StringParser.js
@@ -6,12 +6,12 @@ Seed({
 		describe("Test Mold.Lib.VDom.StringParser", function(){
 
 			it("parse some value nodes", function(){
-				var result = StringParser('<a href="test" > {{irgendwas}} </a>  {{wasanders}} anders');
-
-				expect(result.length).toBe(5);
-				expect(result[0].type).toBe("markup");
-				expect(result[1].type).toBe("node");
-				expect(result[1].name).toBe("irgendwas");
+				var result = StringParser('{{#block}} <a href="test" > {{irgendwas}} </a>  {{wasanders}} anders {{/block}} s');
+				
+				expect(result.length).toBe(3);
+				expect(result[0].type).toBe(8);
+				expect(result[1].type).toBe(2);
+				expect(result[1].name).toBe('block');
 			});
 
 
@@ -19,26 +19,21 @@ Seed({
 				var result = StringParser('<a href="test"> {{#myblock}}  test value {{content}} in block {{/myblock}} </a>');
 				expect(result.length).toBe(3);
 				expect(result[1].name).toBe("myblock");
-				expect(result[1].value).toBe("  test value {{content}} in block ")
 				
 			})
 
 			it("parse some value nodes with block content and negative block", function(){
 				var result = StringParser('<a href="test"> {{#myblock}}  test value {{content}} in block {{/myblock}} {{^myblock}} if not definnde {{/myblock}} </a>');
-				expect(result.length).toBe(5);
-				expect(result[1].name).toBe("myblock");
-				expect(result[1].value).toBe("  test value {{content}} in block ")
-				expect(result[3].name).toBe("myblock");
-				expect(result[3].nodeType).toBe(3);
+				expect(result.length).toBe(4);
 			})
 
 			it("parse list in a list", function(){
 				var result = StringParser('<a href="test"> {{#myblock}}  test {{#insidelist}}  asdasd {{content}} asd {{/insidelist}} {{/myblock}} {{^myblock}} if not definnde {{/myblock}} </a>');
-				expect(result.length).toBe(5);
+				expect(result.length).toBe(4);
 			})
 
-			xit("parse invalid list and throw an error", function(){
-				expect(StringParser('<a href="test"> {{#myblock}}  test {{#insidelist}}  asdasd {{content}} asd {{/ins}} </a>')).toThrowError();
+			it("parse invalid list and throw an error", function(){
+				expect(function() { StringParser('<a href="test"> {{#myblock}}  test {{#insidelist}}  asdasd {{content}} asd {{/ins}} </a>') }).toThrowError();
 			})
 
 		})

0e14c1eb7ae4ac9935196437c17d22038ac44cd7
jailkey <jan.kaufmann@netzproduktionen.de>
2015-09-06 18:56:27 +0200

    diable triggerSave on publish

--- Mold/Lib/Observer.js
+++ Mold/Lib/Observer.js
@@ -9,10 +9,17 @@ Seed({
 	function(){
 		var events = new Mold.Lib.Event(this);
 		Mold.mixin(this, events);
+
+		var _publish = function(e, data, conf){
+			conf = conf || {};
+			conf.disableSaveTrigger = true;
+			this.trigger(e, data, conf)
+		}
+
 		return {
-			pub : this.trigger,
+			pub : _publish,
 			sub : this.on,
-			publish : this.trigger,
+			publish : _publish,
 			subscribe : this.on,
 			at : this.at,
 			on : this.on,

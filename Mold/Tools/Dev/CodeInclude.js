21b24ab2ffa2a7883c089e4208a5e4d9ec455200
jailkey <jan.kaufmann@netzproduktionen.de>
2015-09-16 18:53:15 +0200

    add include as feature

--- Mold/Tools/Dev/CodeInclude.js
+++ Mold/Tools/Dev/CodeInclude.js
@@ -6,11 +6,36 @@ Seed({
 	
 		Mold.addPreProcessor("codeinclude", function(code){
 			code = code.replace(/\/\/°include(.*?)$/gm, function(match, contents){
-				var seed = Mold.getSeed(Mold.trim(contents));
+				var before = "";
+				var as = false;
+				if(contents && contents.indexOf(" as ")){
+					var parts = contents.split(" as ");
+					contents = parts[0];
+					as = Mold.trim(parts[1]);
+					if(as && ~as.indexOf(".")){
+						var asParts = as.split(".");
+						var collected = "";
+						before = "var "
+						for(var i = 0; i < asParts.length - 1; i++){
+							before += collected + asParts[i] + " = " + collected + asParts[i] + " || {};\n";
+							collected += collected + asParts[i] + ".";
+						}
+					}
+					
+				}
+				var seed = Mold.getRawSeed(Mold.trim(contents)).func;
+	
 				if(typeof seed === "function"){
-					var output = seed.toString();
-					output = output.substring(output.indexOf("{") + 1, output.lastIndexOf("}"));
-			
+					if(as){
+						if(before){
+							var output =  before + as + " = " + seed.toString() + ";\n";
+						}else{
+							var output =  "var " + as + " = " + seed.toString() + ";\n";
+						}
+					}else{
+						var output = seed.toString();
+						output = output.substring(output.indexOf("{") + 1, output.lastIndexOf("}"));
+					}
 					return output;
 				}else{
 					return 'console.log("' + contents+ ' is not a function!");'

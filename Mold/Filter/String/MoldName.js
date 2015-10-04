976a1bc44dddc76e6b0d394231e9de37cba08644
jailkey <jan.kaufmann@netzproduktionen.de>
2015-10-04 23:09:56 +0200

    add filter

---
+++ Mold/Filter/String/MoldName.js
@@ -0,0 +1,19 @@
+Seed({
+		name : "Mold.Filter.String.MoldName",
+		dna : "filter",
+		include : [
+			"Mold.DNA.Filter"
+		],
+		filterName :  "moldname",
+	},
+	function(data, config){
+
+
+		if(Mold.isArray(data)){
+
+		}
+
+
+		return data;
+	}
+)
